1b408e220c8a93e77d4f16d92eebb041b06127b0
Crysiswar <adriwarch@hotmail.fr>
2019-04-15 16:31:01 +0200

    modification framework: only API

--- prism/app/Controllers/AgendaController.php
+++ prism/app/Controllers/AgendaController.php
@@ -2,7 +2,6 @@
 
 namespace PrismGestion\Controllers;
 
-use Slim\Views\Twig as View;
 use PrismGestion\Models\Materiel;
 
 class AgendaController extends Controller
@@ -10,6 +9,6 @@ class AgendaController extends Controller
 
     public function index($request, $response)
     {
-        return $this->view->render($response,'home.twig');
+        return 'haha';
     }
 }
