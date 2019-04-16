de3535e0693f9fb4dd1187762b762f42ef3822ae
Crysiswar <adriwarch@hotmail.fr>
2019-04-16 16:10:47 +0200

    docker-compose.yml: ajout charlylab.app, filldatabase: ajout de données dans la table Materiel

---
+++ sql/fill_database.sql
@@ -0,0 +1,10 @@
+INSERT INTO `type`(`nom`) VALUES('photo');
+
+INSERT INTO `materiel` (`constructeur`,`modele`,`description`,`nb_ex`,`type`,`created_at`) VALUES ('Canon','EOS X5', 'appareil photo classique', 1, 1,'2019-04-16');
+INSERT INTO `materiel` (`constructeur`,`modele`,`description`,`nb_ex`,`type`,`created_at`) VALUES ('Canon','EOS X5', 'appareil photo classique', 1, 1,'2019-04-16');
+INSERT INTO `materiel` (`constructeur`,`modele`,`description`,`nb_ex`,`type`,`created_at`) VALUES ('Canon','EOS X5', 'appareil photo classique', 1, 1,'2019-04-16');
+INSERT INTO `materiel` (`constructeur`,`modele`,`description`,`nb_ex`,`type`,`created_at`) VALUES ('Canon','EOS X5', 'appareil photo classique', 1, 1,'2019-04-16');
+INSERT INTO `materiel` (`constructeur`,`modele`,`description`,`nb_ex`,`type`,`created_at`) VALUES ('Canon','EOS X5', 'appareil photo classique', 1, 1,'2019-04-16');
+INSERT INTO `materiel` (`constructeur`,`modele`,`description`,`nb_ex`,`type`,`created_at`) VALUES ('Canon','EOS X5', 'appareil photo classique', 1, 1,'2019-04-16');
+INSERT INTO `materiel` (`constructeur`,`modele`,`description`,`nb_ex`,`type`,`created_at`) VALUES ('Canon','EOS X5', 'appareil photo classique', 1, 1,'2019-04-16');
+INSERT INTO `materiel` (`constructeur`,`modele`,`description`,`nb_ex`,`type`,`created_at`) VALUES ('Canon','EOS X5', 'appareil photo classique', 1, 1,'2019-04-16');
