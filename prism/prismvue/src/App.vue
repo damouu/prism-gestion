f5d8e1bdfe3051d3288a4f607d135a08fed0dbc2
Crysiswar <adriwarch@hotmail.fr>
2019-04-23 14:39:31 +0200

    mise en place du vueJS

---
+++ prism/prismvue/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div id="nav">
+
+        <b-navbar toggleable="lg" type="dark" variant="danger">
+
+          <b-container>
+
+            <b-navbar-brand href="#">Prism</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+              <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto">
+                  <b-nav-item class="mr-5" href="#">Inventaire</b-nav-item>
+                  <b-nav-item class="mr-5" href="#">Inventaire</b-nav-item>
+                  <b-nav-item class="mr-5" href="#">Inventaire</b-nav-item>
+                  <b-nav-item class="mr-5" href="#">Inventaire</b-nav-item>
+                  <b-nav-item class="mr-5" href="#">Inventaire</b-nav-item>
+                </b-navbar-nav>
+              </b-collapse>
+
+          </b-container>
+
+        </b-navbar>
+    </div>
+
+
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+  @import 'node_modules/bootstrap/scss/bootstrap';
+  @import 'node_modules/bootstrap-vue/src/index.scss';
+
+#app {
+
+}
+</style>
