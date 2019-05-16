4756d6b6912a14e88665ad9ba0191dd6b7bd4a0f
Crysiswar <adriwarch@hotmail.fr>
2019-05-16 15:34:06 +0200

    ADD: Validation ExemplaireController

--- prism/src/app/Controllers/ExemplaireController.php
+++ prism/src/app/Controllers/ExemplaireController.php
@@ -10,9 +10,14 @@ use PrismGestion\Models\Type;
 use PrismGestion\Utils\ResponseWriter;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Respect\Validation\Exceptions\NestedValidationException;
+use Respect\Validation\Validator as v;
+use stdClass;
 
 class ExemplaireController extends Controller
 {
+
+
 
     public function getAll(Request $request, Response $response, $args)
     {
@@ -191,7 +196,22 @@ class ExemplaireController extends Controller
     {
 
         $content = $request->getParsedBody();
+
         $materiel = Materiel::where('id','=',$content['materiel'])->first();
+
+        $postValidateMateriel = v::notOptional()->intType()->length(1,11);
+        $postValidateReference = v::notOptional()->StringType()->length(1,20);
+        $postValidateEtat = v::notOptional()->stringType();
+        $postValidateFournisseur = v::notOptional()->intType()->length(1,11);
+        $postValidatePrixHt = v::notOptional()->floatType();
+        $postValidatePrixTtc = v::notOptional()->floatType();
+        $postValidateNumSerie = v::notOptional()->StringType()->length(1,128);
+        $postValidateFinancement = v::notOptional()->StringType()->length(1,128);
+        $postValidateBonCommande = v::notOptional()->StringType()->length(1,128);
+        $postValidateImmobilisation = v::Optional(v::StringType()->length(1,128));
+        $postValidateUrl = v::Optional(v::url());
+        $postValidateDateAchat = v::date('Y-m-d');
+        $postValidateStockage = v::notOptional()->StringType()->length(1,128);
 
         if(
             !isset($content['materiel'])
@@ -213,32 +233,70 @@ class ExemplaireController extends Controller
         {
             $data = ApiErrors::NotFound($request->getUri());
         }else{
+
+            $postValeur = new stdClass;
+            $postValeur->materiel = $content['materiel'];
+            $postValeur->reference = $content['reference'];
+            $postValeur->etat = trim($content['etat']);
+            $postValeur->fournisseur = $content['fournisseur'];
+            $postValeur->prix_ht = $content['prix_ht'];
+            $postValeur->prix_ttc = $content['prix_ttc'];
+            $postValeur->num_serie = trim($content['num_serie']);
+            $postValeur->financement = trim($content['financement']);
+            $postValeur->bon_commande = trim($content['bon_commande']);
+            if(isset($content["immobilisation"])){
+                $postValeur->immobilisation = trim($content['immobilisation']);
+            }
+            else
+            {
+                $postValeur->immobilisation = null;
+            }
+            if(isset($content["url"])){
+                $postValeur->url = trim($content['url']);
+            }
+            else
+            {
+                $postValeur->url = null;
+            }
+            $postValeur->date_achat = trim($content['date_achat']);
+            $postValeur->stockage = trim($content['stockage']);
+
             try
             {
-                DB::transaction(function () use ($content) {
 
-                    $count = Exemplaire::where('materiel','=',$content['materiel'])->withTrashed()->count();
+                $postValidateMateriel->assert($postValeur->materiel);
+                $postValidateReference->assert($postValeur->reference);
+                $postValidateEtat->assert($postValeur->etat);
+                $postValidateFournisseur->assert($postValeur->fournisseur);
+                $postValidatePrixHt->assert($postValeur->prix_ht);
+                $postValidatePrixTtc->assert($postValeur->prix_ttc);
+                $postValidateNumSerie->assert($postValeur->num_serie);
+                $postValidateFinancement->assert($postValeur->financement);
+                $postValidateBonCommande->assert($postValeur->bon_commande);
+                $postValidateImmobilisation->assert($postValeur->immobilisation);
+                $postValidateUrl->assert($postValeur->url);
+                $postValidateDateAchat->assert($postValeur->date_achat);
+                $postValidateStockage->assert($postValeur->stockage);
+
+                DB::transaction(function () use ($postValeur) {
+
+                    $count = Exemplaire::where('materiel','=',$postValeur->materiel)->withTrashed()->count();
                     $exemplaire = new Exemplaire();
-                    $exemplaire->materiel = $content['materiel'];
-                    $exemplaire->reference = $content['reference'];
-                    $exemplaire->fournisseur = $content['fournisseur'];
-                    $exemplaire->prix_ht = $content['prix_ht'];
-                    $exemplaire->prix_ttc = $content['prix_ttc'];
-                    $exemplaire->num_serie = $content['num_serie'];
-                    $exemplaire->financement = $content['financement'];
-                    $exemplaire->bon_commande = $content['bon_commande'];
-                    $exemplaire->stockage = $content['stockage'];
-                    if(isset($content["url"])){
-                        $exemplaire->url = $content['url'];
-                    }
-                    if(isset($content["immobilisation"])){
-                        $exemplaire->immobilisation = $content['immobilisation'];
-                    }
-                    $exemplaire->etat = $content['etat'];
-                    $exemplaire->date_achat = $content['date_achat'];
+                    $exemplaire->materiel = $postValeur->materiel;
+                    $exemplaire->reference = $postValeur->reference;
+                    $exemplaire->fournisseur = $postValeur->fournisseur;
+                    $exemplaire->prix_ht = $postValeur->prix_ht;
+                    $exemplaire->prix_ttc = $postValeur->prix_ttc;
+                    $exemplaire->num_serie = $postValeur->num_serie;
+                    $exemplaire->financement = $postValeur->financement;
+                    $exemplaire->bon_commande = $postValeur->bon_commande;
+                    $exemplaire->stockage = $postValeur->stockage;
+                    $exemplaire->url = $postValeur->url;
+                    $exemplaire->immobilisation = $postValeur->immobilisation;
+                    $exemplaire->etat = $postValeur->etat;
+                    $exemplaire->date_achat = $postValeur->date_achat;
                     $exemplaire->num_ex = ($count)+1;
                     $exemplaire->save();
-
                 });
 
                 $data = [
@@ -247,10 +305,15 @@ class ExemplaireController extends Controller
                     'exemplaire' => 'reussi'
                 ];
             }
+            catch(NestedValidationException $e)
+            {
+                $data = ApiErrors::ValidationError($e->getMessages());
+            }
             catch(\Exception $e)
             {
                 $data = ApiErrors::InternalError();
             }
+
         }
 
         return ResponseWriter::ResponseWriter($response, $data);
@@ -265,6 +328,20 @@ class ExemplaireController extends Controller
         $content = $request->getParsedBody();
 
         $exemplaire = Exemplaire::find($id);
+
+        $putValidateMateriel = v::notOptional()->intType()->length(1,11);
+        $putValidateReference = v::notOptional()->StringType()->length(1,20);
+        $putValidateEtat = v::notOptional()->stringType();
+        $putValidateFournisseur = v::notOptional()->intType()->length(1,11);
+        $putValidatePrixHt = v::notOptional()->floatType();
+        $putValidatePrixTtc = v::notOptional()->floatType();
+        $putValidateNumSerie = v::notOptional()->StringType()->length(1,128);
+        $putValidateFinancement = v::notOptional()->StringType()->length(1,128);
+        $putValidateBonCommande = v::notOptional()->StringType()->length(1,128);
+        $putValidateImmobilisation = v::Optional(v::StringType()->length(1,128));
+        $putValidateUrl = v::Optional(v::url());
+        $putValidateDateAchat = v::date('Y-m-d');
+        $putValidateStockage = v::notOptional()->StringType()->length(1,128);
 
 
         if(
@@ -285,7 +362,35 @@ class ExemplaireController extends Controller
         }
         else if(empty($exemplaire))
         {
-            $materiel = Materiel::where('id','=',$content['materiel'])->first();
+
+            $putValeur = new stdClass;
+            $putValeur->materiel = $content['materiel'];
+            $putValeur->reference = $content['reference'];
+            $putValeur->etat = trim($content['etat']);
+            $putValeur->fournisseur = $content['fournisseur'];
+            $putValeur->prix_ht = $content['prix_ht'];
+            $putValeur->prix_ttc = $content['prix_ttc'];
+            $putValeur->num_serie = trim($content['num_serie']);
+            $putValeur->financement = trim($content['financement']);
+            $putValeur->bon_commande = trim($content['bon_commande']);
+            if(isset($content["immobilisation"])){
+                $putValeur->immobilisation = trim($content['immobilisation']);
+            }
+            else
+            {
+                $putValeur->immobilisation = null;
+            }
+            if(isset($content["url"])){
+                $putValeur->url = trim($content['url']);
+            }
+            else
+            {
+                $putValeur->url = null;
+            }
+            $putValeur->date_achat = trim($content['date_achat']);
+            $putValeur->stockage = trim($content['stockage']);
+
+            $materiel = Materiel::where('id','=',$putValeur->materiel)->first();
 
             if(empty($materiel))
             {
@@ -293,29 +398,39 @@ class ExemplaireController extends Controller
             }else{
                 try
                 {
-                    DB::transaction(function () use ($content) {
+                    $putValidateMateriel->assert($putValeur->materiel);
+                    $putValidateReference->assert($putValeur->reference);
+                    $putValidateEtat->assert($putValeur->etat);
+                    $putValidateFournisseur->assert($putValeur->fournisseur);
+                    $putValidatePrixHt->assert($putValeur->prix_ht);
+                    $putValidatePrixTtc->assert($putValeur->prix_ttc);
+                    $putValidateNumSerie->assert($putValeur->num_serie);
+                    $putValidateFinancement->assert($putValeur->financement);
+                    $putValidateBonCommande->assert($putValeur->bon_commande);
+                    $putValidateImmobilisation->assert($putValeur->immobilisation);
+                    $putValidateUrl->assert($putValeur->url);
+                    $putValidateDateAchat->assert($putValeur->date_achat);
+                    $putValidateStockage->assert($putValeur->stockage);
 
-                        $count = Exemplaire::where('materiel','=',$content['materiel'])->withTrashed()->count();
-                        $count1 = Exemplaire::where('materiel','=',$content['materiel'])->count();
+                    DB::transaction(function () use ($putValeur) {
+
+                        $count = Exemplaire::where('materiel','=',$putValeur->materiel)->withTrashed()->count();
                         $exemplaire = new Exemplaire();
-                        $exemplaire->materiel = $content['materiel'];
-                        $exemplaire->reference = $content['reference'];
-                        $exemplaire->etat = $content['etat'];
-                        $exemplaire->fournisseur = $content['fournisseur'];
-                        $exemplaire->prix_ht = $content['prix_ht'];
-                        $exemplaire->prix_ttc = $content['prix_ttc'];
-                        $exemplaire->num_serie = $content['num_serie'];
-                        $exemplaire->financement = $content['financement'];
-                        $exemplaire->bon_commande = $content['bon_commande'];
-                        $exemplaire->stockage = $content['stockage'];
-                        if(isset($content["url"])){
-                            $exemplaire->url = $content['url'];
-                        }
+                        $exemplaire->materiel = $putValeur->materiel;
+                        $exemplaire->reference = $putValeur->reference;
+                        $exemplaire->fournisseur = $putValeur->fournisseur;
+                        $exemplaire->prix_ht = $putValeur->prix_ht;
+                        $exemplaire->prix_ttc = $putValeur->prix_ttc;
+                        $exemplaire->num_serie = $putValeur->num_serie;
+                        $exemplaire->financement = $putValeur->financement;
+                        $exemplaire->bon_commande = $putValeur->bon_commande;
+                        $exemplaire->stockage = $putValeur->stockage;
+                        $exemplaire->url = $putValeur->url;
+                        $exemplaire->immobilisation = $putValeur->immobilisation;
+                        $exemplaire->etat = $putValeur->etat;
+                        $exemplaire->date_achat = $putValeur->date_achat;
                         $exemplaire->num_ex = ($count)+1;
                         $exemplaire->save();
-                        $materiel = Materiel::where('id','=',$content['materiel'])->first();
-                        $materiel->nb_ex = ($count1)+1;
-                        $materiel->save();
                     });
 
                     $data = [
@@ -323,6 +438,10 @@ class ExemplaireController extends Controller
                         'code' => 201,
                         'exemplaire' => 'reussi'
                     ];
+                }
+                catch(NestedValidationException $e)
+                {
+                    $data = ApiErrors::ValidationError($e->getMessages());
                 }
                 catch(\Exception $e)
                 {
@@ -332,18 +451,60 @@ class ExemplaireController extends Controller
         }
         else
         {
+            $putValeur = new stdClass;
+            $putValeur->materiel = $content['materiel'];
+            $putValeur->reference = $content['reference'];
+            $putValeur->etat = trim($content['etat']);
+            $putValeur->fournisseur = $content['fournisseur'];
+            $putValeur->prix_ht = $content['prix_ht'];
+            $putValeur->prix_ttc = $content['prix_ttc'];
+            $putValeur->num_serie = trim($content['num_serie']);
+            $putValeur->financement = trim($content['financement']);
+            $putValeur->bon_commande = trim($content['bon_commande']);
+            if(isset($content["immobilisation"])){
+                $putValeur->immobilisation = trim($content['immobilisation']);
+            }
+            else
+            {
+                $putValeur->immobilisation = null;
+            }
+            if(isset($content["url"])){
+                $putValeur->url = trim($content['url']);
+            }
+            else
+            {
+                $putValeur->url = null;
+            }
+            $putValeur->date_achat = trim($content['date_achat']);
+            $putValeur->stockage = trim($content['stockage']);
+
             try{
 
-                $exemplaire->materiel = $content['materiel'];
-                $exemplaire->reference = $content['reference'];
-                $exemplaire->etat = $content['etat'];
-                $exemplaire->stockage = $content['stockage'];
-                $exemplaire->fournisseur = $content['fournisseur'];
-                $exemplaire->prix_achat = $content['prix_achat'];
-                $exemplaire->num_serie = $content['num_serie'];
-                if(isset($content["url"])){
-                    $exemplaire->url = $content['url'];
-                }
+                $putValidateReference->assert($putValeur->reference);
+                $putValidateEtat->assert($putValeur->etat);
+                $putValidateFournisseur->assert($putValeur->fournisseur);
+                $putValidatePrixHt->assert($putValeur->prix_ht);
+                $putValidatePrixTtc->assert($putValeur->prix_ttc);
+                $putValidateNumSerie->assert($putValeur->num_serie);
+                $putValidateFinancement->assert($putValeur->financement);
+                $putValidateBonCommande->assert($putValeur->bon_commande);
+                $putValidateImmobilisation->assert($putValeur->immobilisation);
+                $putValidateUrl->assert($putValeur->url);
+                $putValidateDateAchat->assert($putValeur->date_achat);
+                $putValidateStockage->assert($putValeur->stockage);
+
+                $exemplaire->reference = $putValeur->reference;
+                $exemplaire->fournisseur = $putValeur->fournisseur;
+                $exemplaire->prix_ht = $putValeur->prix_ht;
+                $exemplaire->prix_ttc = $putValeur->prix_ttc;
+                $exemplaire->num_serie = $putValeur->num_serie;
+                $exemplaire->financement = $putValeur->financement;
+                $exemplaire->bon_commande = $putValeur->bon_commande;
+                $exemplaire->stockage = $putValeur->stockage;
+                $exemplaire->url = $putValeur->url;
+                $exemplaire->immobilisation = $putValeur->immobilisation;
+                $exemplaire->etat = $putValeur->etat;
+                $exemplaire->date_achat = $putValeur->date_achat;
                 $exemplaire->save();
 
                 $data = [
@@ -352,7 +513,11 @@ class ExemplaireController extends Controller
                     'exemplaire' => $exemplaire
                 ];
             }
-            catch(\Exception$e)
+            catch(NestedValidationException $e)
+            {
+                $data = ApiErrors::ValidationError($e->getMessages());
+            }
+            catch(\Exception $e)
             {
                 $data = ApiErrors::InternalError();
             }
@@ -360,6 +525,9 @@ class ExemplaireController extends Controller
 
         return ResponseWriter::ResponseWriter($response, $data);
     }
+
+
+
 
     public function patch(Request $request, Response $response, $args)
     {
@@ -373,31 +541,7 @@ class ExemplaireController extends Controller
         {
             try {
 
-                if($params['select'] === 'service')
-                {
-                    $etat = $request->getParsedBody();
-                    $exemplaire = Exemplaire::find($id);
-
-                    if(!isset($etat['etat'])){
-                        $data = ApiErrors::BadRequest();
-                    }
-                    else if(empty($exemplaire))
-                    {
-                        $data = ApiErrors::NotFound($request->getUri());
-                    }
-                    else
-                    {
-                        $exemplaire->etat = $etat['etat'];
-                        $exemplaire->save();
-                        $data = [
-                            'type' => "success",
-                            'code' => 200,
-                            'exemplaire' => $exemplaire
-                        ];
-                    }
-
-                }
-                else if ($params['select'] === 'reforme')
+                if ($params['select'] === 'reforme')
                 {
                     $exemplaire = Exemplaire::onlyTrashed()->find($id);
                     $materiel = Materiel::withTrashed()->find($exemplaire->materiel);
