e6be5d3020de43fea04b24ddd6d00dd904d4d67e
Crysiswar <adriwarch@hotmail.fr>
2019-06-20 15:07:33 +0200

    ADD: modal on click evenement

--- prism/prismvue/src/components/agenda/AgendaEmprunts.vue
+++ prism/prismvue/src/components/agenda/AgendaEmprunts.vue
@@ -28,6 +28,55 @@
                 />
             </b-col>
         </b-row>
+
+        <b-modal
+                id="bv-modal-eventClick"
+                hide-footer
+                refs="bv-modal-eventClick"
+        centered scrollable>
+            <template slot="modal-header" slot-scope="">
+                <h5>Information sur la feuille reservation n°<router-link :to="{path:'/reservation/'+modalClick.reservation}" target="_blank">{{modalClick.id}}</router-link></h5>
+            </template>
+            <div>
+                <div>
+                    <h4>Debut réservation: {{modalClick.debut_resa}}</h4>
+                    <h4>Fin réservation: {{modalClick.fin_resa}}</h4>
+                </div>
+                <div>
+                    <h4>Observations:</h4>
+                    <p>{{modalClick.observation}}</p>
+                </div>
+                <div>
+                    <h4>Exemplaires:</h4>
+                    <b-table
+                            :items="modalClick.materiels"
+                            striped hover
+                            :fields="fieldsMateriel"
+                            show-empty
+                    >
+                        <template slot="detail" slot-scope="row">
+                            <b-button variant="info" @click="row.toggleDetails" v-if="row.detailsShowing"><font-awesome-icon :icon="['fas','sort-down']" /></b-button>
+                            <b-button variant="info" @click="row.toggleDetails" v-if="!row.detailsShowing"><font-awesome-icon :icon="['fas','caret-right']" /></b-button>
+                        </template>
+                        <template slot="row-details" slot-scope="row">
+                            <b-card>
+                                <b-table
+                                        striped hover
+                                        :fields="fieldsExemplaire"
+                                        :items="row.item.exemplaires"
+                                        show-empty
+                                ></b-table>
+                            </b-card>
+                        </template>
+                    </b-table>
+                </div>
+
+
+
+            </div>
+
+        </b-modal>
+
     </div>
 </template>
 <script>
@@ -61,6 +110,18 @@
                         next: 'suivant',
                     },
                 loading:false,
+                modalClick:[],
+
+                fieldsMateriel: [
+                    {key:'detail',sortable:false},
+                    {key:'constructeur', sortable:true},
+                    {key:'modele', sortable:true},
+                ],
+                fieldsExemplaire: [
+                    {key:'reference', sortable:true},
+                    {key:'num_ex', sortable:true},
+                    {key:'num_serie', sortable:true},
+                ],
             }
         },
         mounted(){
@@ -78,8 +139,21 @@
                             end.setHours(end.getHours()+1);
                             let ended = new Date(element.date_retour);
                             ended.setHours(end.getHours()+1);
-                            this.calendarEvents.push({title: 'reservation n°'+element.reservation, start: element.date_depart, end:end, id: element.id, color:"green"});
-                            this.calendarEvents.push({title: 'reservation n°'+element.reservation, start:element.date_retour, end:ended, id:element.id, color:"red"});
+
+                            this.calendarEvents.push({
+                                title: element.id,
+                                start: element.date_depart,
+                                end:end, color:"green",
+                                otherDatas:{
+                                    debut_resa:element.date_depart,
+                                    fin_resa:element.date_retour,
+                                    id: element.id, exemplaires:element.exemplaires,
+                                    observation: element.observation,
+                                    reservation:element.reservation
+                                }
+                            });
+
+                            this.calendarEvents.push({title: element.id, start:element.date_retour, end:ended, color:"red", otherDatas:{debut_resa:element.date_depart,fin_resa:element.date_retour,id: element.id, exemplaires:element.exemplaires, observation: element.observation, reservation:element.reservation}});
                         })
                         this.loading=false;
                     })
@@ -88,9 +162,41 @@
                     })
             },
             handleDateClick(arg) {
-                console.log(arg.event);
-                console.log(arg.el);
-                console.log(arg.jsEvent)
+
+                this.modalClick= {
+                    observation: arg.event._def.extendedProps.otherDatas.observation,
+                    materiels: arg.event._def.extendedProps.otherDatas.exemplaires,
+                    id: arg.event._def.extendedProps.otherDatas.id,
+                    debut_resa:arg.event._def.extendedProps.otherDatas.debut_resa,
+                    fin_resa:arg.event._def.extendedProps.otherDatas.fin_resa,
+                    reservation:arg.event._def.extendedProps.otherDatas.reservation,
+                };
+                let materiel = [];
+                this.modalClick.materiels.forEach(element => {
+                    if(materiel!=undefined){
+                        let exist = false;
+                        materiel.forEach(test => {
+                            if(test.id===element.materiel.id){
+                                exist=true;
+                            }
+                        });
+                        if(exist!==true){
+                            materiel.push(element.materiel);
+                        }
+                    }
+                });
+
+                materiel.forEach(element=>{
+                    element.exemplaires=[];
+                    this.modalClick.materiels.forEach( value => {
+                        if(value.materiel.id===element.id){
+                            element.exemplaires.push({id:value.id,num_ex:value.num_ex, reference:value.reference, num_serie:value.num_serie});
+                        }
+                    })
+                });
+
+                this.modalClick.materiels = materiel;
+                this.$bvModal.show('bv-modal-eventClick')
             },
         }
 
