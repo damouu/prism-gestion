488c878a67ff54761a8f46bdcba44aaa069f2b98
Damou_ <mouadsehbaoui@gmail.com>
2020-07-15 16:53:59 +0200

    ajout authentification JWT

---
+++ prism/src/app/Middlewares/decodeJWT.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PrismGestion\Middlewares;
+
+use Firebase\JWT\JWT;
+use \lbs\common\bootstrap\Eloquent;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\SignatureInvalidException;
+use Firebase\JWT\BeforeValidException;
+
+class decodeJWT
+{
+    function decodeJWT(Request $rq, Response $rs, callable $next)
+    {
+        try {
+            $token = JWT::decode($rq->getAttribute("token"), "secret", ['HS512']);
+            $rq = $rq->withAttribute("token", $token);
+            return $next($rq, $rs);
+        } catch (ExpiredException $e) {
+            return $rs;
+        } catch (SignatureInvalidException $e) {
+            return $rs;
+        } catch (BeforeValidException $e) {
+            return $rs;
+        } catch (\UnexpectedValueException $e) {
+            return $rs;
+        }
+    }
+
+}
