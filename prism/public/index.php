8aee9b47a1da88d5d9c59e2673c34eb579f264e5
Damou_ <mouadsehbaoui@gmail.com>
2020-07-22 20:39:53 +0200

    structure index.php, creation JWT via phpCAS dans index.php

--- prism/public/index.php
+++ prism/public/index.php
@@ -2,10 +2,68 @@
 
 require __DIR__ . '/../src/vendor/autoload.php';
 
+use Firebase\JWT\JWT;
+
+use PrismGestion\Models\Users as users;
+
+phpCAS::setDebug();
+
+phpCAS::setVerbose(false);
+
+phpCAS::client(CAS_VERSION_2_0, "auth.univ-lorraine.fr", 443, "", true);
+
+phpCAS::setNoCasServerValidation();
+
+phpCAS::forceAuthentication();
+
 $loader = new \Twig\Loader\FilesystemLoader('../src/templates');
 
 $twig = new Twig\Environment($loader, [
     'cache' => false
 ]);
 
-echo $twig->render('index.twig', ["uNetID" => "sehbaoui1"]);
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+        'db' => parse_ini_file('../src/bootstrap/conf.ini')
+    ],
+
+]);
+
+$container = $app->getContainer();
+
+$capsule = new \Illuminate\Database\Capsule\Manager();
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$user = users::where("NetID", '=', phpCAS::getUser())->first();
+
+
+if (!empty($user)) {
+    $token = JWT::encode(
+        ['iss' => 'https://iutnc-resamat.univ-lorraine.fr',
+            'aud' => 'https://iutnc-resamat.univ-lorraine.fr',
+            'iat' => time(),
+            'exp' => time() + 3600,
+            'uNetID' => $user->NetID,
+            'lvl' => $user->NetID_Access_level
+        ],
+        "secret", 'HS512');
+} else {
+    $token = JWT::encode(
+        ['iss' => 'https://iutnc-resamat.univ-lorraine.fr',
+            'aud' => 'https://iutnc-resamat.univ-lorraine.fr',
+            'iat' => time(),
+            'exp' => time() + 3600,
+            'uNetID' => phpCAS::getUser(),
+            'lvl' => 1
+        ],
+        "secret", 'HS512');
+}
+try {
+    echo $twig->render('index.twig', ["JWT" => $token]);
+} catch (\Twig\Error\LoaderError $e) {
+} catch (\Twig\Error\RuntimeError $e) {
+} catch (\Twig\Error\SyntaxError $e) {
+}
