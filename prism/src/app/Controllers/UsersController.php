dad4fea725fc1c3a2fdaffc6c5239e836fd24d6f
Damou_ <mouadsehbaoui@gmail.com>
2020-07-22 20:28:46 +0200

    suppression method userSignIn() -> UsersController

--- prism/src/app/Controllers/UsersController.php
+++ prism/src/app/Controllers/UsersController.php
@@ -14,45 +14,6 @@ use Firebase\JWT\JWT;
 class UsersController extends Controller
 {
 
-    public function userSignIn(Request $request, Response $response, $args)
-    {
-        $user = users::where("NetID", '=', $request->getQueryParams("NetID"))->first();
-        if (!empty($user)) {
-            $token = JWT::encode(
-                ['iss' => 'https://iutnc-resamat.univ-lorraine.fr',
-                    'aud' => 'https://iutnc-resamat.univ-lorraine.fr',
-                    'iat' => time(),
-                    'exp' => time() + 3600,
-                    'uNetID' => $user->NetID,
-                    'lvl' => $user->NetID_Access_level
-                ],
-                "secret", 'HS512');
-            $data = [
-                'type' => "success",
-                'code' => 200,
-                'JWT' => $token
-            ];
-            return ResponseWriter::ResponseWriter($response, $data);
-        } else {
-            $token = JWT::encode(
-                ['iss' => 'https://iutnc-resamat.univ-lorraine.fr',
-                    'aud' => 'https://iutnc-resamat.univ-lorraine.fr',
-                    'iat' => time(),
-                    'exp' => time() + 3600,
-                    'uNetID' => $request->getQueryParams("NetID"),
-                    'lvl' => 1
-                ],
-                "secret", 'HS512');
-            $data = [
-                'type' => "success",
-                'code' => 200,
-                'JWT' => $token
-            ];
-            return ResponseWriter::ResponseWriter($response, $data);
-        }
-
-
-    }
 
     public function printJWT(Request $request, Response $response, $args)
     {
