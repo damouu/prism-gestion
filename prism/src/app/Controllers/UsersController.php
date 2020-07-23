b6f14b19a7f78d3c5af84439c9c0aa443efdafad
Damou_ <mouadsehbaoui@gmail.com>
2020-07-23 14:00:59 +0200

    ajout fonctionnalite update user

--- prism/src/app/Controllers/UsersController.php
+++ prism/src/app/Controllers/UsersController.php
@@ -2,13 +2,15 @@
 
 namespace PrismGestion\Controllers;
 
-use PrismGestion\Controllers\Controller;
+
 use PrismGestion\Errors\ApiErrors;
 use PrismGestion\Models\Users as users;
 use PrismGestion\Utils\ResponseWriter;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Firebase\JWT\JWT;
+use Respect\Validation\Exceptions\NestedValidationException;
+use Respect\Validation\Validator as v;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 
 class UsersController extends Controller
@@ -36,6 +38,43 @@ class UsersController extends Controller
         ];
         return ResponseWriter::ResponseWriter($response, $data);
     }
+
+    public function updateUser(Request $request, Response $response, $args)
+    {
+        $id = intval($args['id']);
+        $content = $request->getParsedBody();
+        $postValidateNetID_Access_level = v::notOptional()->intType();
+        if (is_int($id)) {
+            if (isset($content['NetID_Access_level']) and (in_array($content['NetID_Access_level'], range(1, 3)))) {
+                try {
+                    $user = users::findOrFail($args['id']);
+                    $postValidateNetID_Access_level->assert($content['NetID_Access_level']);
+                    $user->NetID_Access_level = filter_var($content['NetID_Access_level'], FILTER_VALIDATE_INT);
+                    $user->save();
+                    $data = [
+                        'type' => "success",
+                        'code' => 200,
+                        'user' => $user
+                    ];
+                } catch (ModelNotFoundException $ex) {
+                    $data = [
+                        'type' => "error",
+                        'code' => 405,
+                    ];
+                } catch (NestedValidationException $e) {
+                    $data = ApiErrors::ValidationError($e->getMessages());
+                } catch (\Exception $e) {
+                    $data = ApiErrors::InternalError();
+                }
+            } else {
+                $data = ApiErrors::BadRequest();
+            }
+        } else {
+            $data = ApiErrors::BadRequest();
+        }
+        return ResponseWriter::ResponseWriter($response, $data);
+    }
+
 
     public function deleteUser(Request $request, Response $response, $args)
     {
