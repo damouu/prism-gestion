e197dd143440f860d47c9aad2bb0c06d8c562624
damou_ <mouadsehbaoui@gmail.com>
2021-05-19 19:25:19 +0200

    refactor: CoursController.php getCoursUUID
    modifie la fonction getCoursUUID pour jeter une exception dans le cas ou le uuid ne correspond a acunne ressource existant dans la base de donnees.
    
    #PG-5 Done Unassigned Done Done

--- prism/src/app/Controllers/CoursController.php
+++ prism/src/app/Controllers/CoursController.php
@@ -5,7 +5,6 @@ namespace PrismGestion\Controllers;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use PrismGestion\Models\Cours;
-use PrismGestion\Utils\ResponseWriter;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -49,14 +48,17 @@ class CoursController extends Controller
 
     public function getCoursUUID(Request $request, Response $response, $args): \Slim\Http\Response
     {
-        $cours = cours::where('uuid', '=', $args['uuid'])->firstOrFail();
-        $prof = $cours->professeur;
-        $data = [
-            'type' => "success",
-            'code' => 200,
-            'prof' => $prof,
-            'cours' => $cours
-        ];
-        return ResponseWriter::ResponseWriter($response, $data);
+        try {
+            $cours = cours::where('uuid', '=', $args['uuid'])->firstOrFail();
+            $prof = $cours->professeur;
+            $data = [
+                'type' => "success",
+                'code' => 200,
+                'cours' => $cours
+            ];
+        } catch (ModelNotFoundException $modelNotFoundException) {
+            $data = $this->ApiErrors->NotFound($modelNotFoundException->getCode());
+        }
+        return $this->ResponseWriter->responseWriter($response, $data);
     }
 }
