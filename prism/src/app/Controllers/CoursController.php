1332fb10f194593c9b49e9991670bf454af59b42
damou_ <mouadsehbaoui@gmail.com>
2021-05-19 19:12:02 +0200

    feature: CoursController.php
    modifie le cors de la fonction pour prendre en compte les queryParams si ils sont envoye dans l'uri ou sion retourne tous les cours.
    
    #PG-4 Done Unassigned Done Done

--- prism/src/app/Controllers/CoursController.php
+++ prism/src/app/Controllers/CoursController.php
@@ -3,6 +3,7 @@
 
 namespace PrismGestion\Controllers;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use PrismGestion\Models\Cours;
 use PrismGestion\Utils\ResponseWriter;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -10,15 +11,40 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class CoursController extends Controller
 {
-    public function getAllCours(Request $request, Response $response, $args): \Slim\Http\Response
+    public function getAllCours(Request $request, Response $response, $args)
     {
-        $cours = cours::find(1);
-        $data = [
-            'type' => "success",
-            'code' => 200,
-            'users' => $cours
-        ];
-        return ResponseWriter::ResponseWriter($response, $data);
+        $queryParams = $request->getQueryParams();
+        if (isset($queryParams['intitule'])) {
+            try {
+                $cours = cours::where('intitule', 'LIKE', '%' . $queryParams['intitule'] . '%')->firstOrFail();
+                $data = [
+                    'type' => "success",
+                    'code' => 200,
+                    'cours' => $cours
+                ];
+            } catch (ModelNotFoundException $modelNotFoundException) {
+                $data = $this->ApiErrors->NotFound($modelNotFoundException->getCode());
+            }
+        } elseif (isset($queryParams['salle'])) {
+            try {
+                $cours = cours::where('salle', '=', $queryParams['salle'])->firstOrFail();
+                $data = [
+                    'type' => "success",
+                    'code' => 200,
+                    'cours' => $cours
+                ];
+            } catch (ModelNotFoundException $modelNotFoundException) {
+                $data = $this->ApiErrors->NotFound($modelNotFoundException->getCode());
+            }
+        } else {
+            $cours = cours::all();
+            $data = [
+                'type' => "success",
+                'code' => 200,
+                'cours' => $cours
+            ];
+        }
+        return $this->ResponseWriter->responseWriter($response, $data);
     }
 
     public function getCoursUUID(Request $request, Response $response, $args): \Slim\Http\Response
