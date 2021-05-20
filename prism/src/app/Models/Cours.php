7743cfd81412523df53844a258578a27195272a9
damou_ <mouadsehbaoui@gmail.com>
2021-05-20 16:47:03 +0200

    Refactor: Cours
    ajout de la propriety pour soft delete.
    
    #PG-6 Done Unassigned Fixed Done

--- prism/src/app/Models/Cours.php
+++ prism/src/app/Models/Cours.php
@@ -3,12 +3,15 @@
 namespace PrismGestion\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cours extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'cours';
     public $timestamps = false;
-     protected $hidden = array('id', 'professeur_id');
+    protected $hidden = array('id', 'professeur_id');
 
     public function professeur()
     {
