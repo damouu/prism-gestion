392685eb1959b544dca12b9cfe6105bd7029b1bf
damou_ <mouadsehbaoui@gmail.com>
2021-05-21 14:04:28 +0200

    refactor, ajout plus informations phpdoc.

--- prism/src/app/Middlewares/CoursValidators.php
+++ prism/src/app/Middlewares/CoursValidators.php
@@ -3,12 +3,14 @@
 namespace PrismGestion\Middlewares;
 
 use Exception;
+use JsonException;
 use Respect\Validation\Validator;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 /**
  * Class CoursValidators
+ * @author damou
  * @package PrismGestion\Middlewares
  */
 class CoursValidators
@@ -18,10 +20,10 @@ class CoursValidators
      * @param Response $response
      * @param callable $next
      * @return Response
+     * @throws JsonException
      */
-    public function __invoke(Request $request, Response $response, callable $next)
+    public function __invoke(Request $request, Response $response, callable $next): Response
     {
-
         $content = $request->getParsedBody();
         $postValidationIntitule = Validator::notOptional()->StringType()->length(1, 200);
         $postValidationSalle = Validator::notOptional()->intType()->length(1, 5);
@@ -33,7 +35,7 @@ class CoursValidators
                 ->withStatus(405)
                 ->withHeader('Content-Type', 'application/json; charset=utf8');
             $resp->getBody()
-                ->write(json_encode('Une ou plusieurs validations n\'ont pas été respectées.'));
+                ->write(json_encode('Une ou plusieurs validations n\'ont pas été respectées.', JSON_THROW_ON_ERROR));
             return $resp;
         }
         return $next($request, $response);
